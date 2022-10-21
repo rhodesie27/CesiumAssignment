034d1e508f1100177c8d5116c2cbe717b96a8ca9
rhodesie27 <76666642+rhodesie27@users.noreply.github.com>
2022-10-21 18:11:46 -0400

    Initial commit

---
+++ Source/CesiumAssignment/CesiumAssignmentGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CesiumAssignmentGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CESIUMASSIGNMENT_API ACesiumAssignmentGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
